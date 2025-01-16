6022e35f27ac8215ccd6fb91155126119e2a4d91
Brynjar <Brynjar>
2025-01-16 13:07:28 +0100

    started working on the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import ProfileHeader from "./components/ProfileHeader";
 
 export default function App() {
-  return(
-    <div>
-      
+  return (
+    <div className="flex flex-col items-center  bg-zinc-900 p-6 text-white h-screen">
+      <ProfileHeader />
+      <h1 className="text-red-500">Hello</h1>
     </div>
-  )
+  );
 }
--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -4,24 +4,24 @@ import { FileText, Linkedin, Github } from 'lucide-react';
 const ProfileHeader = () => {
   return (
     <div className="bg-zinc-900 p-6 text-white">
-      <h1 className="text-4xl font-bold mb-2">Prasoon Mahawar</h1>
+      <h1 className="text-4xl font-bold mb-2">Brynjar Berg Ødegaard</h1>
       
       <div className="space-y-1 mb-4">
-        <p className="text-gray-400 text-xl">Software Engineer</p>
+        <p className="text-gray-400 text-xl">Web developer</p>
         <p className="text-gray-400 text-xl">F1 Enthusiast</p>
       </div>
       
       <div className="flex items-center text-gray-400 mb-6">
         <div className="flex items-center gap-2">
           <div className="w-5 h-5 rounded-full bg-gray-400" />
-          <span>Rajasthan, India</span>
+          <span>Asker, Akershus</span>
         </div>
       </div>
       
       <div className="flex gap-4">
         <button className="flex items-center gap-2 bg-zinc-800 hover:bg-zinc-700 px-4 py-2 rounded-md">
           <FileText size={20} />
-          <span>Resume</span>
+          <span>CV</span>
         </button>
         
         <div className="flex gap-4">
